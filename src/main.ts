c9da79f22619aca11110bc1ab15bef4aa1cc1e39
stephenmcardle <smcardle1025@gmail.com>
2026-01-05 21:08:49 -0500

    Change font to noto sans

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import '@/icons/fontawesome';
 import router from './router';
 import '@/style.css';
 
+import '@fontsource/noto-sans/400.css';
+import '@fontsource/noto-sans/500.css';
+import '@fontsource/noto-sans/700.css';
+
 const app = createApp(App)
   .component('font-awesome-icon', FontAwesomeIcon)
   .use(router)
