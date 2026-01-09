8cc2ab5333f4c19b34b5b548f8cd30b336f899fc
stephenmcardle <smcardle1025@gmail.com>
2026-01-09 14:26:57 -0500

    add github base URL

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  base: 'https://stephenmcardle.github.com/stephenmcardle.com',
   build: {
     rollupOptions: {
       output: {
