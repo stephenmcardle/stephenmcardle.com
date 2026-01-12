67bf58fee796eec354c30951b5e715ebd76c4428
stephenmcardle <smcardle1025@gmail.com>
2026-01-12 08:41:07 -0500

    update base url

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: '/personal-website/',
+  base: '/',
   build: {
     rollupOptions: {
       output: {
