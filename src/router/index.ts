c03ca56ac9ff778de787dc1c39266ddc9eaddaa8
stephenmcardle <smcardle1025@gmail.com>
2026-01-13 13:53:20 -0500

    add dynamic titles to router

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,17 +7,31 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: () => import('@/views/HomePage.vue'),
+      meta: {
+        title: 'Home',
+      },
     },
     {
       path: '/sketches',
       name: 'sketches',
       component: () => import('@/views/SketchesPage.vue'),
+      meta: {
+        title: 'Sketches',
+      },
     },
     {
       path: '/:pathMatch(.*)*',
-      redirect: { name: 'home' }
+      redirect: { name: 'home', params: {} }
     },
   ],
+});
+
+const DEFAULT_TITLE = 'Stephen McArdle'
+router.afterEach((to) => {
+  document.title =
+    to.meta.title
+    ? `${to.meta.title} | ${DEFAULT_TITLE}`
+    : DEFAULT_TITLE;
 });
 
 export default router;
